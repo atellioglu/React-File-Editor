462c94bbbb8fd676517d67d65ab96115501e6c83
Abdullah Tellioglu <abdullahtellioglu93@gmail.com>
2019-12-09 22:28:11 +0300

    first commit

---
+++ src/component/FileEditor/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button, Editor, Node } from 'component';
+
+export default class FileEditor extends React.Component {
+  render() {
+    return (
+      <div className="test" />
+    );
+  }
+}
---
+++ src/component/index.js
@@ -0,0 +1,3 @@
+export * from './Button';
+export * from './Editor';
+export * from './Node';
